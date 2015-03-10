19299c3e5d603b8abc2c06dd4ddddbd00d2285db
Anna McGeachy <anna.mcgeachy@gmail.com>
2015-03-09 22:16:25 -0700

    i don't even know when i last committed these was.

--- processing unique orfs.R
+++ processing unique orfs.R
@@ -150,24 +150,28 @@ reading_frame_single_intron = function(inputfile, dataset_name){
   up$exon_percents
   head(up$no_introns_both)
   head(up$zero_two)
+  nrow(up$zero_two)
   
   down = reading_frame_single_intron("down_inside_orf_unique.bed", "down")
   typeof(down)
   down$exon_percents
   head(down$no_introns_both)
   head(down$zero_two)
+  nrow(down$zero_two)
   
   no_recomb = reading_frame_single_intron("no_recomb_inside_orf_unique.bed", "no_recomb")
   typeof(no_recomb)
   no_recomb$exon_percents
   head(no_recomb$no_introns_both)
   head(no_recomb$zero_two)
+  nrow(no_recomb$zero_two)
   
   post_recomb = reading_frame_single_intron("post_recomb_inside_orf_unique.bed", "post_recomb")
   typeof(post_recomb)
   post_recomb$exon_percents
   head(post_recomb$no_introns_both)
   head(post_recomb$zero_two)
+  nrow(post_recomb$zero_two)
 
 #####
 
@@ -212,7 +216,7 @@ reading_frame_single_intron = function(inputfile, dataset_name){
     down_duplicates =lapply(down_dup, function(x) down$no_introns_both[which(down$no_introns_both$unique==x),])
 
   #take the unique fragments and put them into one table
-    all_uniques = c(up_unique, down_unique, no_recomb_unique, post_recomb_unique)
+    all_uniques = c(up_unique, down_unique, post_recomb_unique)
     all_uniques = as.data.frame(unique(all_uniques))
     colnames(all_uniques) = c("identifiers")
     head(all_uniques)
@@ -226,6 +230,7 @@ reading_frame_single_intron = function(inputfile, dataset_name){
     head(all_uniques)
     typeof(all_uniques$up_frag)
     all_uniques[is.na(all_uniques)] = 0 #set NAs to 0
+    write.csv(all_uniques, "all unique frag with counts per sample.csv")
 
   #find the highest count up and down (ends up being a glycolysis enzyme and PAT1 (decapping) respectively)
     all_uniques[which(all_uniques$up_frag==max(all_uniques$up_frag)),"identifiers"]
@@ -292,44 +297,141 @@ boxplot(enriched_in_down$up_frag, enriched_in_down$down_frag, xaxt='n', ylab='lo
 axis(1, at=1:2, labels=c("up_frag", "down_frag"))
 dev.off()
 
-#look at the max enriched
-head(enriched_in_up)
+#make ratio of fragment counts
 enriched_in_up$ratio = enriched_in_up$up_frag/enriched_in_up$down_frag
-
-summary(enriched_in_up$ratio)
 enriched_in_down$ratio = enriched_in_down$down_frag/enriched_in_down$up_frag
+
+#pull the highest count 
+#picks those with the highest ratio, that being inf (because the alt strain has 0 counts)
 enriched_in_up[which(enriched_in_up$up_frag==max(enriched_in_up[which(enriched_in_up$ratio==max(enriched_in_up$ratio)),"up_frag"])),]
 enriched_in_down[which(enriched_in_down$down_frag==max(enriched_in_down[which(enriched_in_down$ratio==max(enriched_in_down$ratio)),"down_frag"])),]
+#picks the highest count, period (alt strain count not = 0 )
 enriched_in_down[which(enriched_in_down$down_frag==max(enriched_in_down$down_frag)),]
 enriched_in_up[which(enriched_in_up$up_frag==max(enriched_in_up$up_frag)),]
-enriched_in_up[which(enriched_in_up$ratio==max(enriched_in_up$ratio)),"up_frag"]
 
-head(up$no_introns_both)
-dim(enriched_in_up)
-head(enriched_in_up)
+#read in data file with xref potential (has gene names and descriptions)
+xref = read.delim("SGD_features.tab", header=FALSE, quote="")
 
+  #up
+    #make a table with all of the fragment information (from no_intron_both) with enrichment information (10X, 100X, and ratio)
+    enriched_up_more_info = up$no_introns_both[match(enriched_in_up$identifiers, up$no_introns_both$unique),]
+    enriched_up_more_info$enriched_in_up_10x = enriched_in_up[match(enriched_up_more_info$unique, enriched_in_up$identifiers),"enriched_in_up_10x"]
+    enriched_up_more_info$enriched_in_up_100x = enriched_in_up[match(enriched_up_more_info$unique, enriched_in_up$identifiers),"enriched_in_up_100x"]
+    enriched_up_more_info$ratio = enriched_in_up[match(enriched_up_more_info$unique, enriched_in_up$identifiers),"ratio"]
+    
+    #now cross reference it with xref to add gene description
+    enriched_up_more_info$gene_useful = xref[match(enriched_up_more_info$gene_name, xref$V4),"V5"]
+    enriched_up_more_info$gene_desc = xref[match(enriched_up_more_info$gene_name, xref$V4),"V16"]
 
-?order
+    #then reorder it based on ratio of enrichment
+    enriched_up_more_info = enriched_up_more_info[order(enriched_up_more_info$ratio),]
+    head(enriched_up_more_info)
+    write.csv(enriched_up_more_info, "up enriched fragments 20150218.csv")
 
-enriched_up_all_info = up$no_introns_both[match(enriched_in_up$identifiers, up$no_introns_both$unique),]
-enriched_up_in_frame = up$zero_two[!is.na(match(enriched_in_up$identifiers, up$zero_two$unique)),]
-head(enriched_in_up)
-head(enriched_up_all_info)
-head(enriched_up_in_frame)
+    #now make a frag count per gene table
+    frag_per_gene_up = as.data.frame(table(enriched_up_more_info$gene_name))
+    frag_per_gene_up = frag_per_gene_up[which(frag_per_gene_up$Freq!=0),]
+    frag_per_gene_up = frag_per_gene_up[order(frag_per_gene_up$Freq, decreasing = TRUE),]
+    frag_per_gene_up$gene_useful = xref[match(frag_per_gene_up$Var1, xref$V4), "V5"]
+    frag_per_gene_up$desc = xref[match(frag_per_gene_up$Var1, xref$V4), "V16"]
+    head(frag_per_gene_up)
+    write.csv(frag_per_gene_up, "up frag table count.csv")
 
-dim(up$no_introns_both)
-dim(enriched_in_up)
-dim(enriched_up_in_frame)
+  #down
+    #make a table with all of the fragment information (from no_intron_both) with enrichment information (10X, 100X, and ratio)
+    enriched_down_more_info = down$no_introns_both[match(enriched_in_down$identifiers, down$no_introns_both$unique),]
+    enriched_down_more_info$enriched_in_down_10x = enriched_in_down[match(enriched_down_more_info$unique, enriched_in_down$identifiers),"enriched_in_down_10x"]
+    enriched_down_more_info$enriched_in_down_100x = enriched_in_down[match(enriched_down_more_info$unique, enriched_in_down$identifiers),"enriched_in_down_100x"]
+    enriched_down_more_info$ratio = enriched_in_down[match(enriched_down_more_info$unique, enriched_in_down$identifiers),"ratio"]
+    
+    #now cross reference it with xref to add gene description
+    enriched_down_more_info$gene_useful = xref[match(enriched_down_more_info$gene_name, xref$V4),"V5"]
+    enriched_down_more_info$gene_desc = xref[match(enriched_down_more_info$gene_name, xref$V4),"V16"]
+    
+    #then reorder it based on ratio of enrichment
+    enriched_down_more_info = enriched_down_more_info[order(enriched_down_more_info$ratio),]
+    head(enriched_down_more_info)
+    write.csv(enriched_down_more_info, "down enriched fragments 20150218.csv")
+    
+  #now make a frag count per gene table
+    frag_per_gene_down = as.data.frame(table(enriched_down_more_info$gene_name))
+    frag_per_gene_down = frag_per_gene_down[which(frag_per_gene_down$Freq!=0),]
+    frag_per_gene_down = frag_per_gene_down[order(frag_per_gene_down$Freq, decreasing = TRUE),]
+    frag_per_gene_down$gene_useful = xref[match(frag_per_gene_down$Var1, xref$V4), "V5"]
+    frag_per_gene_down$desc = xref[match(frag_per_gene_down$Var1, xref$V4), "V16"]
+    head(frag_per_gene_down)
+    write.csv(frag_per_gene_down, "down frag table count.csv")
 
-blah = head(enriched_in_up)
-match(enriched_in_up$identifiers, up$zero_two$unique)
-head(up$zero_two)
+#subset the above by those that are in frame (0,2)
 up$zero_two[,"unique"] = paste(up$zero_two$chr_read, up$zero_two$start_read, 
-                                      up$zero_two$end_read, up$zero_two$strand_read, sep="_")
-blah
-blah[order(blah$up_frag),]
+                               up$zero_two$end_read, up$zero_two$strand_read, sep="_")
+down$zero_two[,"unique"] = paste(down$zero_two$chr_read, down$zero_two$start_read, 
+                                 down$zero_two$end_read, down$zero_two$strand_read, sep="_")
 
-?unique
+up_zero_two_list  = match(up$zero_two$unique, enriched_up_more_info$unique)
+up_zero_two_list_noNA = up_zero_two_list[!is.na(up_zero_two_list)]
+enriched_up_zero_two = enriched_up_more_info[up_zero_two_list_noNA,]
+enriched_up_zero_two = enriched_up_zero_two[order(enriched_up_zero_two$ratio),]
+head(enriched_up_zero_two)
+dim(enriched_up_more_info)
+dim(enriched_up_zero_two)
+write.csv(enriched_up_zero_two, "in frame up enriched.csv")
+
+#make fragment table for this as well
+up_zero_two_frag = as.data.frame(table(enriched_up_zero_two$gene_name))
+up_zero_two_frag = up_zero_two_frag[which(up_zero_two_frag$Freq!=0),]
+up_zero_two_frag = up_zero_two_frag[order(up_zero_two_frag$Freq, decreasing = TRUE),]
+up_zero_two_frag$gene_useful = xref[match(up_zero_two_frag$Var1, xref$V4), "V5"]
+up_zero_two_frag$desc = xref[match(up_zero_two_frag$Var1, xref$V4), "V16"]
+write.csv(up_zero_two_frag, "in frame up fragments table.csv")
+
+down_zero_two_list  = match(down$zero_two$unique, enriched_down_more_info$unique)
+down_zero_two_list_noNA = down_zero_two_list[!is.na(down_zero_two_list)]
+enriched_down_zero_two = enriched_down_more_info[down_zero_two_list_noNA,]
+enriched_down_zero_two = enriched_down_zero_two[order(enriched_down_zero_two$ratio),]
+head(enriched_down_zero_two)
+dim(enriched_down_more_info)
+dim(enriched_down_zero_two)
+write.csv(enriched_down_zero_two, "in frame down enriched.csv")
+
+#make fragment table for this as well
+down_zero_two_frag = as.data.frame(table(enriched_down_zero_two$gene_name))
+down_zero_two_frag = down_zero_two_frag[which(down_zero_two_frag$Freq!=0),]
+down_zero_two_frag = down_zero_two_frag[order(down_zero_two_frag$Freq, decreasing = TRUE),]
+down_zero_two_frag$gene_useful = xref[match(down_zero_two_frag$Var1, xref$V4), "V5"]
+down_zero_two_frag$desc = xref[match(down_zero_two_frag$Var1, xref$V4), "V16"]
+write.csv(down_zero_two_frag, "in frame down fragments table.csv")
+
+thing = unique(post_recomb$no_introns_both$gene_name)
+thing = thing[which(thing)]
+write.csv(thing, "all genes.csv")
+
+head(all_uniques)
+summary(all_uniques$up_frag)
+hist(as.data.frame(table(all_uniques[which(all_uniques$up_frag>3 & all_uniques$up_frag<100),"up_frag"])))
+
+?hist
+all_uniques$up_frag>3
+#now look at these (up and down) versus post recombination
+head(all_uniques)
+up_v_post = a_v_b_counts(all_uniques, "up_frag", "post_recomb_frag")
+down_v_post = a_v_b_counts(all_uniques, "down_frag", "post_recomb_frag")
+
+head(up_v_post)
+
+
+YMR295C = enriched_down_zero_two[which(enriched_down_zero_two$gene_name=="YMR295C"),] #most enriched in down 
+YMR295C[,c("read_start_in_cds", "read_end_in_cds")]+ 1
+
+YPR204W = enriched_up_zero_two[which(enriched_up_zero_two$gene_name=="YPR204W"),] #most enriched in up
+YPR204W[,c("read_start_in_cds", "read_end_in_cds")] + 1
+
+YAL038W = enriched_up_zero_two[which(enriched_up_zero_two$gene_name=="YAL038W"),] #PAT1
+YAL038W[,c("read_start_in_cds", "read_end_in_cds")] + 1
+
+YDL160C = enriched_up_zero_two[which(enriched_up_zero_two$gene_name=="YDL160C"),] #DHH1
+(YDL160C[,c("read_start_in_cds", "read_end_in_cds")] + 1) /3
+
 # up$no_introns_both[,"most_unique"] = paste(up$no_introns_both$chr_read, up$no_introns_both$start_read, 
 #                                       up$no_introns_both$end_read, up$no_introns_both$strand_read, 
 #                                       up$no_introns_both$gene_name, sep="_")
