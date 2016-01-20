dcc3af1ba81aa8662979c82dc9fca9d86a5d79d9
Anna McGeachy <anna.mcgeachy@gmail.com>
2016-01-20 13:12:05 -0800

    did the process for down. noticed that the p values arent distributed as expected. also NOTHING is significant in down. try to do m-w using up/down ratio with pseudocounts.

--- m-w testing.R
+++ m-w testing.R
@@ -1,8 +1,6 @@
 getwd()
 setwd("/Users/annamcgeachy/Google Drive/post trx reg data/screen1_miseq_repiped/")
 
-
-
 library("dplyr")
 
 #read in files
@@ -94,6 +92,8 @@ hist(up_p_vals_adj, add=TRUE, col=3)
 up_inframe_genes_actual$mw_p_vals = up_p_vals
 up_inframe_genes_actual$p_adj = up_p_vals_adj
 
+head(up_inframe_genes_actual)
+
 #reorder by smallest adjusted p value
 up_inframe_genes_actual = up_inframe_genes_actual[order(up_inframe_genes_actual$p_adj),]
 table(up_inframe_genes_actual$p_adj)
@@ -104,3 +104,56 @@ up_inframe_genes_actual$gene_desc = xref[match(up_inframe_genes_actual$Var1, xre
 
 head(up_inframe_genes_actual)
 up_inframe_genes_actual[grep("PAT1", up_inframe_genes_actual$gene_useful),]
+
+
+#########
+
+#make a list of all the gene names
+down_inframe_genes = as.data.frame(table(down_inframe$gene_name), ncol=2)
+
+#notice that there are a lot of 0s (so they occur 0 times)
+table(down_inframe_genes$Freq)
+#so where are they coming from
+head(down_inframe_genes)
+grep("Q0050", down_inframe$gene_name) #they in face don't exist in down_inframe
+grep("Q0050", down$gene_name) #looks like they existed in down,
+down[576,] # but are out of frame
+
+#so how do we get rid of these 0's for the in frame so they don't complicate things
+down_inframe_genes_actual = filter(down_inframe_genes, Freq!=0)
+nrow(down_inframe_genes_actual)
+table(down_inframe_genes_actual$Freq) #ok. got all of the 0s removed.
+head(down_inframe_genes_actual)
+head(up_in)
+
+#
+mw_test = function(x){
+  wilcox.test(
+    filter(down_inframe, gene_name==as.character(x["Var1"]))[,"frag_count"],
+    filter(down_inframe, gene_name!=as.character(x["Var1"]))[,"frag_count"])[3][[1]]
+}
+
+#generate the pvalues, then adjust
+down_p_vals = apply(down_inframe_genes_actual, 1, mw_test)
+down_p_vals_adj = p.adjust(down_p_vals)
+
+#histogram of pvals pre and post adjustment
+hist(down_p_vals, col=2)
+hist(down_p_vals_adj, add=TRUE, col=3)
+table(down_p_vals_adj)
+
+#add the pvalues to the data frame
+down_inframe_genes_actual$mw_p_vals = down_p_vals
+down_inframe_genes_actual$p_adj = down_p_vals_adj
+
+#reorder by smallest adjusted p value
+down_inframe_genes_actual = down_inframe_genes_actual[order(down_inframe_genes_actual$p_adj),]
+table(down_inframe_genes_actual$p_adj)
+
+#add in useful gene information 
+down_inframe_genes_actual$gene_useful = xref[match(down_inframe_genes_actual$Var1, xref$V4),"V5"]
+down_inframe_genes_actual$gene_desc = xref[match(down_inframe_genes_actual$Var1, xref$V4),"V16"]
+
+head(down_inframe_genes_actual)
+down_inframe_genes_actual[grep("PAT1", down_inframe_genes_actual$gene_useful),]
+
